f47912406a48450e2864ebbd1a90db830ba5d312
JuliyTem <dubsteprulit1488@mail.ru>
2018-12-27 07:39:35 +0300

    Yeap

--- Laboratornaya/Main.cpp
+++ Laboratornaya/Main.cpp
@@ -1,15 +1,25 @@
-#include "Stack.h"
-//#include "Queue.h"
-//#include "Classes_Functions.h"
+#include "Classes and Functions.h"
 
 int main()
 {
-	std::string str;
-	std::getline(std::cin, str);
+	try
+	{
+		std::string str;
+		std::getline(std::cin, str);
 
-	TLex a(str);
-	a.convert(str);
-	
-	
-	return 0;
+		TLex a(str);
+		a.convert(str);
+		a.Show();
+		a.Checking();
+		a.To_Polish();
+		a.Count();
+
+		return 0;
+	}
+	catch (int a)
+	{
+		if (a == -3) std::cout << "\n Nothing to count.";
+		if (a == -1) std::cout << "\n Error.";
+		if (a == -2) std::cout << "\n ')number' or ')(' or any without operation.";
+	}
 }
